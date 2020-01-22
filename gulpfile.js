469fbac7d930ef17deb0e155fbbb039c1bb1ae4a
Artem <art.sled.ua@gmail.com>
2020-01-23 00:27:16 +0300

    reboot starter puck.

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,41 @@
-var syntax        = 'sass'; // Syntax: sass or scss;
+const
+    {src, dest, series, watch} = require('gulp'),
+    del = require('del'),
+    sass = require('gulp-sass'),
+    csso = require('gulp-csso'),
+    include = require('gulp-file-include'),
+    htmlMin = require('gulp-htmlmin'),
+    autoPrefix = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
+    sync = require('browser-sync').create();
 
-var gulp          = require('gulp'),
-		gutil         = require('gulp-util' ),
-		sass          = require('gulp-sass'),
-		browsersync   = require('browser-sync'),
-		concat        = require('gulp-concat'),
-		uglify        = require('gulp-uglify'),
-		cleancss      = require('gulp-clean-css'),
-		rename        = require('gulp-rename'),
-		autoprefixer  = require('gulp-autoprefixer'),
-		notify        = require("gulp-notify"),
-		rsync         = require('gulp-rsync');
+function html() {
+    return src('app/**.html')
+        .pipe(include({prefix: '@@'}))
+        .pipe(htmlMin({
+            collapseWhitespace: true
+        }))
+        .pipe(dest('dist'))
+}
+function scss () {
+    return src('app/scss/**.scss')
+        .pipe(sass())
+        .pipe(autoPrefix())
+        .pipe(csso())
+        .pipe(concat('main.min.css'))
+        .pipe(dest('dist/css'))
+}
+function clear() {
+    return del('dist');
+}
+function serve() {
+    sync.init({
+        server: './dist'
+    });
 
-gulp.task('browser-sync', function() {
-	browsersync({
-		server: {
-			baseDir: 'app'
-		},
-		notify: false,
-		// open: false,
-		// tunnel: true,
-		// tunnel: "projectname", //Demonstration page: http://projectname.localtunnel.me
-	})
-});
-
-gulp.task('styles', function() {
-	return gulp.src('app/'+syntax+'/**/*.'+syntax+'')
-	.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
-	.pipe(rename({ suffix: '.min', prefix : '' }))
-	.pipe(autoprefixer(['last 15 versions']))
-	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
-	.pipe(gulp.dest('app/css'))
-	.pipe(browsersync.reload( {stream: true} ))
-});
-
-gulp.task('js', function() {
-	return gulp.src([
-		'app/libs/jquery/dist/jquery.min.js',
-		'app/js/common.js', // Always at the end
-		])
-	.pipe(concat('scripts.min.js'))
-	// .pipe(uglify()) // Mifify js (opt.)
-	.pipe(gulp.dest('app/js'))
-	.pipe(browsersync.reload({ stream: true }))
-});
-
-gulp.task('rsync', function() {
-	return gulp.src('app/**')
-	.pipe(rsync({
-		root: 'app/',
-		hostname: 'username@yousite.com',
-		destination: 'yousite/public_html/',
-		// include: ['*.htaccess'], // Includes files to deploy
-		exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
-		recursive: true,
-		archive: true,
-		silent: false,
-		compress: true
-	}))
-});
-
-gulp.task('watch', ['styles', 'js', 'browser-sync'], function() {
-	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', ['styles']);
-	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
-	gulp.watch('app/*.html', browsersync.reload)
-});
-
-gulp.task('default', ['watch']);
+    watch('app/**.html', series(html)).on('change', sync.reload);
+    watch("app/scss/**.scss", series(scss)).on('change', sync.reload);
+}
+exports.build = series(clear, scss, html);
+exports.devB = series(clear, scss, html, serve);
+exports.clear = clear;
